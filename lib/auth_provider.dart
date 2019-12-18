f91f486e7e363b012f793f846a0c207c51cd340d
Milind Mevada <milind.mevada@solutelabs.com>
2019-12-18 11:58:38 +0530

    Added auth abstract layer and default storage implementation

--- lib/auth_provider.dart
+++ lib/auth_provider.dart
@@ -1,1 +1,9 @@
 library auth_provider;
+
+import 'package:auth_provider/session_storage_impl.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+export 'abstract_auth_provider.dart';
+export 'session_storage.dart';
+
+final defaultSessionStorage = SecureStorage(storage: FlutterSecureStorage());
